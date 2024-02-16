198be0d407492594a34f208fee5144bd5e8f7167
Anki <sainiankitas121@gmail.com>
2024-02-16 11:37:49 +0530

    Login and Register Page

---
+++ Frontend/src/pages/Register.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Alert , Button , Form , Row , Col , Stack } from 'react-bootstrap'
+
+
+const Register = () => {
+  return (
+    <>
+    <Form>
+        <Row style={{
+            height: "100vh",
+            justifyContent: "center",
+            paddingTop: "7%"
+        }}> 
+            <Col xs={6}>
+                <Stack gap={3}>
+                    <h2>Register</h2>
+                    <Form.Control type="text" placeholder='Name' />
+                    <Form.Control type="email" placeholder='Email' />
+                    <Form.Control type="password" placeholder='Password' />
+                    <Button variant='primary' type="submit">
+                        Register
+                    </Button>
+                    <Alert variant='danger'>
+                        <p>An error occured</p>
+                    </Alert>
+                </Stack>
+            </Col>
+        </Row>
+    </Form>
+    </>
+  )
+}
+
+export default Register
